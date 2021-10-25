ac3b234385e37f2e3b614aa0d7f126dbd5030ad1
Oscar Olsson <happybits@gmail.com>
2021-10-25 17:30:22 +0200

    Nytt projekt

---
+++ Hangon/Hangon.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hangon.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
